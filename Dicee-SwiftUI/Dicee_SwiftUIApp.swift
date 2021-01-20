919d72a11a266a9b41da872b6447aff7d4dea68e
michaeldukich <michaeljohndukich@gmail.com>
2021-01-20 17:44:52 -0500

    first commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Michael Dukich on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
